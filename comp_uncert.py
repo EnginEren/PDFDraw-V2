607f6f86a0d2b871b52622d46e9ea10f7aac2b3a
Engin Eren <eren@nafhh-cms05.desy.de>
2015-12-21 15:43:45 +0100

    model uncertainty added

--- comp_uncert.py
+++ comp_uncert.py
@@ -20,6 +20,25 @@ def writeTo(filename, data):
             f.write(str(i))
             f.write('\n')
 
+#function for combining files
+def combine(f,exp, typ):
+    if typ == "exp":
+        combine = "paste -d ' ' store/central-x store/"+ f +  "_01-central-" + exp + " store/"+f + "plus store/" + f + "minus"
+        arrange = "awk '{print $1,$2,($2+$3), ($2-$4)}' > store/" + f + "-EXP-" + exp
+    elif typ == "exp+mod":
+        combine = "paste -d ' ' store/central-x store/"+ f +  "_01-central-" + exp + " store/"+ f + "Exp+Mod-plus store/" + f + "Exp+Mod-minus"
+        arrange = "awk '{print $1,$2,($2+$3), ($2-$4)}' > store/" + f + "-EXP+MOD-" + exp
+
+    p1 = Popen(combine, stdout=PIPE, shell=True)
+    p2 = Popen(arrange, stdin=p1.stdout, shell=True)
+
+    p1.wait()
+    p2.wait()
+
+    #print "File : store/" + f + "-EXP-" + exp + '\n'
+    #print "File : store/" + f + "-EXP+MOD-" + exp + '\n'
+    #print "created.."
+
 
 # Function for "adding in quadrature"
 def addinQ(x):
@@ -60,7 +79,20 @@ def ForExp(source, plus, minus):
             plus.append(max(cent,up,down)-cent)
             minus.append(cent - min(cent,up,down))
 
+#Function for model uncertatiny calculation
 
+def ForMod(source, mplus, mminus):
+    source = "store/" + source
+    with open(source) as f:
+        for i in f:
+            nums = i.split()
+            cent = float(nums[1])
+            up = float(nums[2])
+            down = float(nums[3])
+            mplus.append(max(cent,up,down)-cent)
+            mminus.append(cent - min(cent,up,down))
+
+#For exp uncertainty
 with open("store/list-sources-" + flavor + "-"+ experiment) as f:
     Plist = []
     Mlist = []
@@ -71,6 +103,18 @@ with open("store/list-sources-" + flavor + "-"+ experiment) as f:
         ForExp(i,lp,lm)    #Call funtion for each sources for exp uncertainy
         Plist.append(lp);  #It is a nested list for PLUS uncert. [ [ 101 x values],[ 101 x values], .. 18 source ]
         Mlist.append(lm);  #It is a nested list for MINUS uncert. [ [ 101 x values],[ 101 x values], .. 18 source ]
+
+#For model uncert.
+with open("store/list-sources-model-" + flavor + "-"+ experiment) as g:
+    modelPlist = []
+    modelMlist = []
+    for j in g:
+        Mlp = []
+        Mlm = []
+        j = j.replace("\n","");
+        ForMod(j,Mlp,Mlm)    #Call funtion for each sources for model uncertainy
+        modelPlist.append(Mlp);  #It is a nested list for PLUS uncert. [ [ 101 x values],[ 101 x values], .. 8 source ]
+        modelMlist.append(Mlm);  #It is a nested list for MINUS uncert. [ [ 101 x values],[ 101 x values], .. 8 source ]
 
 
 #Add them in quadrature for total EXP uncertainty
@@ -85,19 +129,41 @@ for k in range(0,101) :
     ExpPlus.append(addinQ(tmpP))
     ExpMinus.append(addinQ(tmpM))
 
+#Add them in quadrature for total MODEL uncertainty
+ModelPlus = []
+ModelMinus = []
+for a in range(0,101) :
+    MtmpP = []
+    MtmpM = []
+    for b in range(0,8):
+        MtmpP.append(Plist[b][a])
+        MtmpM.append(Mlist[b][a])
+    ModelPlus.append(addinQ(MtmpP))
+    ModelMinus.append(addinQ(MtmpM))
+
+
+#Now we need to add EXP and Model in quad
+ExpModelPlus = []
+ExpModelMinus = []
+
+for c in range(0,101):
+    plus = math.sqrt(pow(ModelPlus[c],2) + pow(ExpPlus[c],2))
+    minus = math.sqrt(pow(ModelMinus[c],2) + pow(ExpMinus[c],2))
+    ExpModelPlus.append(plus)
+    ExpModelMinus.append(minus)
+
+
 #write to file
 writeTo('store/'+flavor+'plus', ExpPlus)
 writeTo('store/'+flavor+'minus', ExpMinus)
 
-combine = "paste -d ' ' store/central-x store/"+ flavor +  "_01-central-" + experiment + " store/"+flavor + "plus store/" + flavor + "minus"
-arrange = "awk '{print $1,$2,($2+$3), ($2-$4)}' > store/" + flavor+ "-EXP-" + experiment
+writeTo('store/'+flavor+'Exp+Mod-plus', ExpModelPlus)
+writeTo('store/'+flavor+'Exp+Mod-minus', ExpModelMinus)
 
-p1 = Popen(combine, stdout=PIPE, shell=True)
-p2 = Popen(arrange, stdin=p1.stdout, shell=True)
+combine(flavor,experiment,"exp")
+combine(flavor,experiment,"exp+mod")
 
-p1.wait()
-p2.wait()
 
 relative("store/"+ flavor + "-EXP-"+ experiment, "store/"+ flavor + "-EXP-relative-" + experiment)
-
+relative("store/"+ flavor + "-EXP+MOD-"+ experiment, "store/"+ flavor + "-EXP+MOD-relative-" + experiment)
 
